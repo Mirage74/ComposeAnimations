a9b084f5bc55db59a3a982258853c2ad54c02f04
balex <balexvicx@gmail.com>
2024-05-26 03:26:15 +0200

    Initial commit

---
+++ app/src/main/java/com/balex/composeanimations/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.balex.composeanimations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.balex.composeanimations.ui.screen.Test
+import com.balex.composeanimations.ui.theme.ComposeAnimationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAnimationsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Test()
+                }
+            }
+        }
+    }
+}
